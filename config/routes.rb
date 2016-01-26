0308f8da0841e9c934736d0a5ff953d9c5568613
alex sanudo <asanudo@itglue.com>
2016-01-26 12:14:59 -0800

    Shit's configured amd working finally.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  root 'dudes#index'
-  resources :dudes
+  root 'application#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
