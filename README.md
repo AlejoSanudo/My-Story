7559f1fb6f159f059a08aea1ff6adaec325cfe0b
alex sanudo <asanudo@itglue.com>
2016-08-19 16:11:21 -0700

    Update README

---
+++ README.md
@@ -0,0 +1,5 @@
+# README
+
+Base Rails application for my preferred stack and architecture (mysql forthcoming)
+
+Please fork off of this project to keep this base for future use
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
 
   # authenticated :user do
       root to: "dashboard#show", as: "dashboard"  # => install Devise at some point
-    # end
+  # end
+
 end
