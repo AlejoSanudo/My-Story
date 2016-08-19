2a8c7168bb8e901f81da8c51c9f130ab0ed10a95
alex sanudo <asanudo@itglue.com>
2016-08-19 16:43:40 -0700

    Added DB working - renamed tests to spec. Basic User model

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source 'https://rubygems.org'
 
+gem 'mysql2', '~> 0.3.21'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'mousetrap-rails'
 gem 'rails', '4.2.4'
@@ -7,7 +8,7 @@ gem 'haml'
 gem 'bootstrap-sass', '~> 3.2.0'
 gem 'autoprefixer-rails'
 # Use sqlite3 as the database for Active Record
-gem 'sqlite3'
+# gem 'sqlite3'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160126180506) do
+ActiveRecord::Schema.define(version: 20160819233724) do
 
-  create_table "dudes", force: :cascade do |t|
-    t.string   "name"
-    t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table "users", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.string   "email",      limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
   end
 
 end
