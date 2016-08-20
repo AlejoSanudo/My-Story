b41de880c6444883f8df5650362e80c0478a6f1d
alex sanudo <asanudo@itglue.com>
2016-08-19 17:03:23 -0700

    button with icon helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,5 +7,9 @@ module ApplicationHelper
       }
     """
   end
+
+  def button_with_icon(icon_name, label, href, btn_type=:default, *options)
+    link_to icon(icon_name) + " " + content_tag(:span, label), href, { class: "icnbtn btn btn-#{btn_type}" }.reverse_merge(options.extract_options!)
+  end
   
 end
